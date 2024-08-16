4aaad1c415e9b72a7af1d96f1347a788e80f0d25
Ahmad Zaki <166486850+ahmadzakiyox@users.noreply.github.com>
2024-08-17 05:00:38 +0700

    Update product.js

--- models/product.js
+++ models/product.js
@@ -1,16 +1,16 @@
-// models/Product.js
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    nama: { type: String, required: true },
-    kode: { type: String, required: true, unique: true },
-    harga: { type: Number, required: true },
-    deskripsi: { type: String, required: true },
-    stock: { type: Number, default: 0 },
-    terjual: { type: Number, default: 0 },
-    isDeleted: { type: Boolean, default: false } // Field untuk soft delete
+  nama: { type: String, required: true },
+  kode: { type: String, required: true, unique: true },
+  harga: { type: Number, required: true },
+  deskripsi: { type: String, required: true },
+  stock: { type: Number, default: 0 },
+  terjual: { type: Number, default: 0 },
+  isDeleted: { type: Boolean, default: false } // Field untuk soft delete
 });
 
+// Cek apakah model sudah ada, jika belum buat model baru
 const Product = mongoose.models.Product || mongoose.model('Product', productSchema);
 
 module.exports = Product;
